5848a6cf4d103e8e576b060874ead7861a4ce590
rhdzmota <rhdzmota@gmail.com>
2019-09-09 16:17:42 -0500

    Add scaffold code for recursivity exercise

---
+++ activities/classwork/recursivity/main.py
@@ -0,0 +1,27 @@
+import fire
+import logging
+
+from util import TriangleBuilder
+
+
+class Main(object):
+
+    def message(self, content):
+        logging.info(content)
+
+    def get_element_recursively(self, i, j):
+        pass
+
+    def pascal_triangle_a(self, level, index=0):
+        pass
+
+    def pascal_triangle_b(self, level, index=0):
+        pass
+
+    def pascal_triangle(self, level, option):
+        pass
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    fire.Fire(Main)
---
+++ activities/classwork/recursivity/util.py
@@ -0,0 +1,21 @@
+import functools
+import time
+
+
+class TriangleBuilder(object):
+    CACHE = {}
+
+    def save(self, i, j, value):
+        pass
+
+    def get(self, i, j, default=None):
+        pass
+
+    def create(self, i, j):
+        pass
+
+    def get_or_create(self, i, j):
+        pass
+
+    def get_row(self, index):
+        pass
