f9e896f0e7831cfcaff3acdcd07d7a532be6658f
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-09-18 20:02:11 -0500

    Create recursive implementation

--- activities/classwork/recursivity/main.py
+++ activities/classwork/recursivity/main.py
@@ -6,18 +6,24 @@ from util import TriangleBuilder
 
 class Main(object):
 
-    @staticmethod
-    def message(content):
+    def message(self, content):
         logging.info(content)
 
     def get_element_recursively(self, i, j):
-        pass
-
+        return  if (j>= i or j == 0) else\
+    self.get_element_recursively(i=i-1, j=j) + \
+    self.get_element_recursively(i=i-1, j=j-1) 
+    
     def pascal_triangle_a(self, level, index=0):
-        pass
+        if (index<level):
+            row = [str(self.get_element_recursively(i=index, j=j))
+                   for j in range(index + 1)]
+            print("".join(row))
+            self.pascal_triangle_a(level=level, index=index+1)
 
     def pascal_triangle_b(self, level, index=0):
-        pass
+        if index < level:
+            row buld
 
     def pascal_triangle(self, level, option):
         pass
--- activities/classwork/recursivity/util.py
+++ activities/classwork/recursivity/util.py
@@ -1,21 +1,43 @@
-import functools
-import time
+import fire
+import logging
+
+from util import TriangleBuilder, caching, timeit
+
+logger = logging.getLogger(_name_)
 
 
-class TriangleBuilder(object):
-    CACHE = {}
+class Main(object):
+    builder = TriangleBuilder()
 
-    def save(self, i, j, value):
-        pass
+    @staticmethod
+    def message(content):
+        logging.info(content)
 
-    def get(self, i, j, default=None):
-        pass
+    @caching
+    def get_element_recursively(self, i, j):
+        return 1 if (j >= i or j == 0) else \
+            self.get_element_recursively(i = i-1, j=j) + self.get_element_recursively(i = i-1, j=j-1)
 
-    def create(self, i, j):
-        pass
+    def pascal_triangle_a(self, level, index=0):
+        if index < level:
+            row = [str(self.get_element_recursively(i=index, j=j)) for j in range(index + 1)]
+            print(" ".join(row))
+            self.pascal_triangle_a(level=level, index=index + 1)
 
-    def get_or_create(self, i, j):
-        pass
+    def pascal_triangle_b(self, level, index):
+        if index < level:
+            row = self.builder.get_row(index = index)
+            print(" ".join(row))
+            self.pascal_triangle_b(level,index=index+1)
 
-    def get_row(self, index):
-        pass
+    @timeit(logger)
+    def pascal_triangle(self, level, option):
+        if "a" == option.lower():
+            self.pascal_triangle_a(level=level, index=0)
+        elif "b" == option.lower():
+            self.pascal_triangle_b(level=level, index=0)
+        else:
+            logging.info(f"Option {option} undefined.")
+
+    if _name_ == "_main_":
+        fire.Fire(Main)
