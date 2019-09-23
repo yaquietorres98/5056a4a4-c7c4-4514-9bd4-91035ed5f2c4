b2735dcc2a2d74bc05055a69f20fa10e7698b34f
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-09-23 17:11:27 -0500

    Create amortization solution

--- activities/classwork/amortization/util.py
+++ activities/classwork/amortization/util.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -10,12 +11,51 @@ class Amortization(object):
         self.n = n
 
     def annuity(self):
-        pass
+        annuity = self.amount / ((1 - (1 + self.interest) ** (-self.n)) / self.interest)
+        return annuity
 
     def get_table(self):
-        """Create a pandas dataframe representing the amortization table."""
-        pass
+        Annuity = self.annuity()
+        Principal_l = [0]
+        Interest_l = [0]
+        Annuity_l = [Annuity]
+        Loan_Val_l = [self.amount]
+
+        for j in range(self.n):
+            Annuity_l.append(Annuity_l[-1])
+            Interest_l.append(Loan_Val_l[-1] * (self.interest))
+            Principal_l.append(Annuity_l[-1] - Interest_l[-1])
+            Loan_Val_l.append(Loan_Val_l[-1] - Principal_l[-1])
+
+        table = pd.DataFrame([Principal_l, Interest_l, Annuity_l, Loan_Val_l]).transpose()
+        table = table.rename(columns={0: 'Principal', 1: 'Interest', 2: 'Annuity', 3: 'Loan Value'})
+        return table
+
 
     def get_plot(self):
-        """Create a plot (fig) to visualize at least two variables from the amortization table."""
-        pass
+        table = self.get_table()
+        fig = plt.figure(1)
+        plt.subplot(2, 2, 1)
+        plt.plot(list(np.arange(self.n) + 1), list(table.iloc[1:, 0]))
+        plt.title('Principal')
+        plt.xlabel('Periods')
+        plt.ylabel('Amount $')
+        plt.subplot(2, 2, 2)
+        plt.plot(list(np.arange(self.n) + 1), list(table.iloc[1:, 1]))
+        plt.title('Interest')
+        plt.xlabel('Periods')
+        plt.ylabel('Interest $')
+        plt.subplot(2, 2, 3)
+        plt.plot(list(np.arange(self.n) + 1), list(table.iloc[1:, 2]))
+        plt.title('Annuity')
+        plt.xlabel('Periods')
+        plt.ylabel('Annuity $')
+        plt.subplot(2, 2, 4)
+        plt.plot(list(np.arange(self.n) + 1), list(table.iloc[1:, 3]))
+        plt.title('Loan Value')
+        plt.xlabel('Periods')
+        plt.ylabel('Loan Value $')
+        plt.show()
+        return fig
+
+
