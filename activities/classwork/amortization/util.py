fb977f69bf1cf6f0b8f133833250593ef98dfef1
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-09-25 19:52:30 -0500

    Ctreate amortization correct

--- activities/classwork/amortization/util.py
+++ activities/classwork/amortization/util.py
@@ -10,31 +10,37 @@ class Amortization(object):
         self.interest = interest
         self.n = n
 
+    @property
     def annuity(self):
         annuity = self.amount / ((1 - (1 + self.interest) ** (-self.n)) / self.interest)
         return annuity
 
     def get_table(self):
-        Annuity = self.annuity()
-        Principal_l = [0]
-        Interest_l = [0]
-        Annuity_l = [Annuity]
-        Loan_Val_l = [self.amount]
-
-        for j in range(self.n):
-            Annuity_l.append(Annuity_l[-1])
-            Interest_l.append(Loan_Val_l[-1] * (self.interest))
-            Principal_l.append(Annuity_l[-1] - Interest_l[-1])
-            Loan_Val_l.append(Loan_Val_l[-1] - Principal_l[-1])
-
-        table = pd.DataFrame([Principal_l, Interest_l, Annuity_l, Loan_Val_l]).transpose()
-        table = table.rename(columns={0: 'Principal', 1: 'Interest', 2: 'Annuity', 3: 'Loan Value'})
-        return table
-
+        rows = []
+        loan_value = self.amount
+        payment= self.annuity
+        for i in range (self.n):
+            interest_value = loan_value * self.interest
+            principal = payment - interest_value
+            loan_value = loan_value - principal
+            rows.append({
+                "interest": interest_value,
+                "payment": payment,
+                "principal": principal,
+                "loan_value": loan_value
+            })
+        return pd.DataFrame(rows).rename_axis("period").reset_index()
 
     def get_plot(self):
-        table = self.get_table()
-        fig = plt.figure(1)
+        df=self.get_table()
+        plot = df.plot.bar(x="perios", y=["principal","interest"], stacked=TRUE)
+        fig = plot.get_figure()
+        plt.show()
+        return fig
+
+
+
+
         plt.subplot(2, 2, 1)
         plt.plot(list(np.arange(self.n) + 1), list(table.iloc[1:, 0]))
         plt.title('Principal')
@@ -57,5 +63,4 @@ class Amortization(object):
         plt.ylabel('Loan Value $')
         plt.show()
         return fig
-
 
