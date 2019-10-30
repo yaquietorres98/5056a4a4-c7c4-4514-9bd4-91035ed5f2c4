2015dc1d92f24dc2a31f18fb1f5deba392bd03dc
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-10-30 08:22:58 -0600

    Exercise.sql final

--- activities/classwork/enrollment-database/exercise.sql
+++ activities/classwork/enrollment-database/exercise.sql
@@ -5,19 +5,21 @@
 
 
 -- A) Get all the unique department names.
+SELECT DISTINCT department FROM course;
 
 -- B) Get the top 10 female students (first_name, last_name, age, gpa) with the best GPA scores and order by age (asc).
-
+select first_name, last_name, age, gpa from student where gender='female' order by gpa desc, age asc, first_name asc, last_name asc limit 10;
 -- C) Count the number of male/female students that are at least 25 years old.
-
+select gender as "Gender", count(gender) as "Count" from student where age>=25 group by gender;
 -- D) Get the number of male/female students that were accepted
-
+select *gender , count(gender) from student inner join enrollment on student.id = enrollment.student_id where enrollment.approved=1 group by gender;
 -- E) Get the min, average, and max GPA of the accepted male students that are less than 20 years old.
-
+select min(gpa), avg(gpa), max(gpa) from student inner join enrollment on student.id = enrollment.student_id where enrollment.approved=1 and student.gender='male' and student.age<=20;
+select * from (select gender,min(gpa), avg(gpa), max(gpa)  from student inner join enrollment e on student.id = e.student_id where student.age <= 20 and e.approved = 1 group by gender) gender_aggregate where gender_aggregate.gender = 'male';
 -- F) Get the number of enrollments to courses that take longer than 2 years to finalize.
-
+select count(*) from course c inner join enrollment e on c.id = e.course_id where c.years > 2 and e.approved = 1
 -- G) Get the number of male/female student that will take a course from the 'Statistics' department.
-
+select gender, count(*) from student s inner join enrollment e on s.id = e.student_id inner join course c on e.course_id = c.id where lower(c.department) = 'statistics' group by gender;
 
 /*
  SECTION: Individual exercises
@@ -25,12 +27,12 @@
  */
 
 -- A) Count the number of courses per department
-
+select department, count(*) from course group by department;
 -- B) How many male/female students were accepted?
-
+select gender, count(*) from student s inner join enrollment e on s.id = e.student_id where e.approved=1 group by gender;
 -- C) How many students were accepted per course?
-
+select c.name, count(*) from student s inner join enrollment e on s.id = e.student_id inner join course c on e.course_id = c.id where e.approved = 1 group by name;
 -- D) What's the average age and gpa per course?
-
+select c.name, avg(age), avg(gpa) from student s inner join enrollment e on s.id = e.student_id inner join course c on e.course_id = c.id group by name;
 -- E) Get the average number of years the enrolled (approved) female student will study.
-
+select avg(years) from student s inner join enrollment e on s.id = e.student_id inner join course c on e.course_id = c.id WHERE e.approved=1 and s.gender = 'female' group by gender;
