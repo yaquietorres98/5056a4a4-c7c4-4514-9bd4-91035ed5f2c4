1c2f32a8b466f1d2eb7cba99e4a4e028f70cac7f
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-12-01 01:24:44 -0600

    b-model

--- activities/classwork/scala-intro/src/main/scala/example/b/model/Person.scala
+++ activities/classwork/scala-intro/src/main/scala/example/b/model/Person.scala
@@ -1,5 +1,32 @@
+
 package example.b.model
 
-case class Person() {
+
+
+case class Person(firstName: String, lastName: String, gender: String){
+
+  def greeting(other:Person): String = other.gender match {
+
+    case Person.Gender.male => s"Guten tag, herr ${other.lastName}"
+
+    case Person.Gender.female => s"Guten tag, frau ${other.lastName}"
+
+    case _ => "Guten tag!"
+
+  }
+
+}
+
+object Person {
+
+  object Gender{
+
+    val male = "male"
+
+    val female = "female"
+
+    val undefined = "undefined"
+
+  }
 
 }
