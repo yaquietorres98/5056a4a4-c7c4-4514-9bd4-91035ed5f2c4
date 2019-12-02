6b1c82618df75f3f142d1fe494b9e024bdb8d726
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-12-01 22:41:34 -0600

    Proyecto

--- activities/classwork/scala-intro/src/main/scala/example/a/model/Timestamp.scala
+++ activities/classwork/scala-intro/src/main/scala/example/a/model/Timestamp.scala
@@ -1,36 +1,5 @@
-
-
 package example.a.model
 
-
-
-case class Timestamp(seconds: Int){
-
-  def +(other: Timestamp): Timestamp =
-
-    Timestamp(seconds + other.seconds)
-
-  //def add(other: Timestamp): Timestamp =
-
-  //Timestamp(seconds + other.seconds)
-
-}
-
-
-
-object Timestamp {
-
-  val secondsInHour: Int = 60*60
-
-  val secondsInMinutes: Int = 60
-
-
-
-  def apply(hours:Int, minutes: Int, seconds: Int): Timestamp =
-
-    Timestamp(seconds= secondsInHour*hours + secondsInMinutes*minutes + seconds)
-
-
-
+case class Timestamp() {
 
 }
