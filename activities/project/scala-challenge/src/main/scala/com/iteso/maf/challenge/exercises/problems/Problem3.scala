3a6ef63b35e04eca7e266f932fa8bba6521abe49
yaquietorres98 <yaquiecontreras98@gmail.com>
2019-12-02 21:19:37 -0600

    Proyecto

--- activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Problem3.scala
+++ activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Problem3.scala
@@ -1,44 +1,117 @@
 package com.iteso.maf.challenge.exercises.problems
 
+
+
 import akka.http.scaladsl.server.Route
+
+
 
 case object Problem3 extends Problem {
 
+
+
   /**
+
     * Recursive Pascal Triangle
+
     * Description:
+
     * There are many ways of implementing a pascal triangle. In this particular exercise, the goal is to provide a
+
     * recursive implementation. You need to research the nature of the pascal triangle in
+
     * order to code the solution. Moreover, you need to add the get verb for this endpoint (see prev. exercises).
+
     *
+
     * Hints:
+
     * - Use the htmlResponse(response: String) function instead of complete(_). For further information on the htmlResponse
+
     * function see the Problem trait in the Problem.scala file.
+
     * - Build the triangle as a string using <br> in order to emulate linebreaks.
+
     *
+
     * Key Points:
+
     * > Use a recursive function.
+
     * > The size will always be a positive integer.
+
     *
+
     * Example:
+
     * Get Request: /problems/3?size=3
+
     * Response: 1<br>1 1<br>1 2 1<br>1 3 3 1
+
     */
+
+
 
   val solution: Route = path("3") {
 
+
+
     get {
+
       parameters('size.as[Int]) {
+
         size => {
+
           // <---- Your code starts here. --->
 
-          val challengeResponse: String = ???
+          val number = size
+
+          def pascal(c: Int, r: Int): Int = {
+
+            if (c == 0 || c == r) 1 else {
+
+              pascal(c-1, r-1) + pascal(c, r-1)
+
+            }
+
+          }
+
+          var list_of_pascal_triangle = List[String]()
+
+          for (row <- 0 to number) {
+
+            var list_of_row_numbers = List[String]()
+
+            for (col <- 0 to row)
+
+            list_of_row_numbers = pascal(col, row).toString.reverse :: list_of_row_numbers
+
+            val string_of_row = list_of_row_numbers.mkString(" ")
+
+            list_of_pascal_triangle = ">rb<" + string_of_row  :: list_of_pascal_triangle
+
+          }
+
+          val string_of_pascal_triangle = list_of_pascal_triangle.mkString("")
+
+          val result = string_of_pascal_triangle.reverse.dropRight(4)
+
+          val challengeResponse: String = result.toString
+
           // <---- Your code ends  here. ---->
+
           htmlResponse(challengeResponse)
+
         }
+
       }
+
     }
 
+
+
   }
+
+
 
 }
--- activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Problem5.scala
+++ activities/project/scala-challenge/src/main/scala/com/iteso/maf/challenge/exercises/problems/Problem5.scala
@@ -1,83 +1,201 @@
 package com.iteso.maf.challenge.exercises.problems
 
+
+
 import akka.http.scaladsl.server.Route
+
+
 
 case object Problem5 extends Problem {
 
+
+
   /**
+
     * IntList ADT
+
     * Description:
+
     * In this problem we introduce the concept of traits for data modeling and structural
+
     * recursion with fold. We provide an implementation of a custom LinkedList for Integers (IntList). This pattern
+
     * is commonly known as Sum Type Pattern (see <book: Essential Scala (4.4 The Sum Type Pattern)>).
+
     * This endpoint is expecting 3 integer parameters (a, b, c) and we use our IntLint to return basic operations such as
+
     * sum (sum), length (len), inner multiplication (mult) and a string representation (str) using arrows "->" for
+
     * each item and final element. Your goal is to complete the missing implementations of the code:
+
     *
+
     * A) Implement the length method for our IntList using pattern matching.
+
     * B) Implement the sum method for our IntList using pattern matching.
+
     * C) Generalize the length and sum implementation into a method called fold.
+
     * D) Generalize the fold method to a generic implementation and call it genericFold[B].
+
     * E) Complete the challengeResponse variable with the inner multiplication using the fold method.
+
     * F) Complete the challengeResponse variable with a string representation of the IntList (e.g. a -> b -> c -> )
+
     * using the genericFold method.
+
     *
+
     * Key Points:
+
     * > Note that the methods are implemented in the IntList trait using pattern matching. We could also have
+
     * implemented these for each class/object using inheritance.
+
     *   - For further information on traits and sealed traits see: <https://gist.github.com/mattbarackman/82b1712add45ceffa8ffe56f81b8bcc2>
+
     *   - For further information on algebraic datatypes and AST see: <book: Essential Scala (4._ Modelling Data with Traits)>
+
     * > Scala's collections use a very similar definition of fold.
+
     *   - For further information on generics and fold see: <book: Essential Scala (5.3 Generic Folds for Generic Data)>
+
     *   - For further information on fold, foldRight and foldLeft see: <https://commitlogs.com/2016/09/10/scala-fold-foldleft-and-foldright/>
+
     *
+
     * Examples:
+
     * Get Request: /problems/5?a=5&b=6&c=2
+
     * Response: {"len":3,"sum":13,"mult":60,"str":"5 -> 6 -> 2 -> "}
+
     */
+
+
 
   case class IntListResult(len: Int, sum: Int, mult: Int, str: String)
 
+
+
   sealed trait IntList {
 
+
+
     // Example method that uses pattern matching
+
     def map(f: Int => Int): IntList = this match {
+
       case Final => Final
-      case Item(head, tail) => Item(f(head), tail)
+
+      case Item(head, tail) => Item(f(head), tail.map(f))
+
     }
 
+
+
     // A) Implement the length method
-    def length: Int = ???
+
+    def length: Int = this match {
+
+      case Final => 0
+
+      case Item(head, tail) => tail.length + 1
+
+    }
+
+
 
     // B) Implement the sum method
-    def sum: Int = ???
+
+    def sum: Int = this match{
+
+      case Final => 0
+
+      case Item(head, tail) => head + tail.sum
+
+    }
+
+
 
     // C) Implement a generalization of the above methods and call it fold.
-    // def fold(end: Int, f: ???): Int = ???
+
+    def fold(end: Int, f: (Int, Int) => Int): Int = this match {
+
+      case Final => end
+
+      case Item(head, tail) => f(head, tail.fold(end, f))
+
+    }
+
+    // para length es b + 1 y para sum es a + b cuando f es f(a,b)
+
 
 
     // D) Implement a generic fold (generalization over the fold on C).
-    // def genericFold[B](end: ???, f: ???): B = ???
+
+    def genericFold[B](end: B, f: (Int, B) => B): B = this match {
+
+      case Final => end
+
+      case Item(head, tail) => f(head, tail.genericFold(end, f))
+
+    }
+
+
 
   }
 
+
+
   final object Final extends IntList
+
   final case class Item(head: Int, tail: IntList) extends IntList
 
+
+
   val solution: Route = path("5") {
+
     get {
+
       parameters('a.as[Int], 'b.as[Int], 'c.as[Int]) {
+
         (a, b, c) => {
+
+
 
           val myList = Item(a, Item(b, Item(c, Final)))
 
+
+
           // E) Complete the challengeResponse with the inner multiplication of the list using the fold method.
+
           // F) Use the generic fold to create a string representation of the list.
-          val challengeResponse: IntListResult = ???
+
+          val challengeResponse: IntListResult = {
+
+            val resultLen: Int = myList.length
+
+            val resultSum: Int = myList.sum
+
+            val resultMult: Int = myList.fold(end = 1, f = (num: Int, z: Int) => num*z)
+
+            val resultStr: String = myList.genericFold(end = "", f = (num: Int, z: String) => num.toString + " -> " + z)
+
+            IntListResult(len=resultLen,sum=resultSum,mult= resultMult,str=resultStr)
+
+          }
+
+
 
           complete(challengeResponse)
+
         }
+
       }
+
     }
+
   }
+
 }
